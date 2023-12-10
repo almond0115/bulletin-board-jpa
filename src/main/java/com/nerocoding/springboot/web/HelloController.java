c2a970863eee2b5831c54cdc45b51d2cbc612744
JongHyunJung <jjh19960115@gmail.com>
2023-12-10 18:13:31 +0900

    feat: Hello Controller 롬복 전환하기

--- src/main/java/com/nerocoding/springboot/web/HelloController.java
+++ src/main/java/com/nerocoding/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.nerocoding.springboot.web;
 
+import com.nerocoding.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    /***
+     * @RequestParam - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+     */
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/nerocoding/springboot/web/HelloControllerTest.java
+++ src/test/java/com/nerocoding/springboot/web/HelloControllerTest.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
@@ -21,5 +21,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())             // mvc.perform 결과 검증 : HTTP Header Status 검증 (여기서는 200 인지 아닌지)
                 .andExpect(content().string(hello));    // mvc.perform 결과 검증 : Controller 에서 "hello" 리턴 값 맞는지 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                            .param("name", name)
+                            .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
