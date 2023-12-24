95c4e7c4f3ce8d289dbfcb82f2073a90110dd7ff
JongHyunJung <jjh19960115@gmail.com>
2023-12-25 07:31:37 +0900

    feat: 배포 자동화 구성

--- src/main/java/com/nerocoding/springboot/Application.java
+++ src/main/java/com/nerocoding/springboot/Application.java
@@ -2,12 +2,17 @@ package com.nerocoding.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.system.ApplicationPid;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing			// JPA Auditing
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
