b3038982528ab45330aa9d5dd1111a93fa1a5885
JongHyunJung <jjh19960115@gmail.com>
2023-12-28 02:56:03 +0900

    fix: URL 더블슬래시 허용

--- src/main/java/com/nerocoding/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/nerocoding/springboot/config/auth/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.nerocoding.springboot.config.auth;
 
 import com.nerocoding.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -28,5 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
+    }
+
+    @Bean
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
     }
 }
